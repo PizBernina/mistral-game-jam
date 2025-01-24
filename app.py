e888e888a72cc885ffcda178ad9f554be6d74bbd
Xuan Son Nguyen <son@huggingface.co>
2025-01-24 11:52:14 +0100

    add demo for API KEY

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
+import os
 from fastapi import Request, FastAPI
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 # TODO: hide your API key as a space's SECRET, access it via environment variable
+API_KEY = os.environ.get("API_KEY")
 
 @app.post("/api/test")
 async def api_test(request: Request):
   return {
     "message": "This is a test",
     "what_you_sent": await request.json(),
+    "my_secret_api_key": API_KEY,
   }
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
